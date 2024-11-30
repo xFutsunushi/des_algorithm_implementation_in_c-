bdb3c48f3e620b65a81796389a939294ba929bba
xfutsunushi <swiezy_1990@o2.pl>
2024-11-30 17:25:10 +0100

    Commit 30.11.2024

---
+++ des_encryption.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+// Kroki do szyfrowania DES
+
+// Poprosić użytkownika o ciąg znaków do szyfrowania
+// Podzielenie wejścia na bloki 64 bit
+// Permutacja pocztąkowa
+// Dzielenie na dwie połowy 32 bitowe
+// 16 x wykonana funkcja feistela
+// Permutacja końcowa
+string ciag;
+
+void permutacja(ciag) {
+
+    
+
+}
+
